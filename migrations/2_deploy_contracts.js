b905a52b556c4ebb6dca4ffb38c708295b72e9af
natalia <natalia.gulko@yahoo.com>
2018-08-28 21:44:29 -0700

    Added deploying OperationsProxy contract, updated settings for Sokol

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,20 @@
 "use strict";
 
 const SimpleOperations = artifacts.require("./SimpleOperations.sol");
+const OperationsProxy = artifacts.require("./OperationsProxy.sol");
 
 module.exports = deployer => {
-  deployer.deploy(SimpleOperations);
+  deployer.deploy(SimpleOperations)
+  .then(() => console.log("SimpleOperations address: " + SimpleOperations.address));
+
+  deployer.deploy(OperationsProxy,
+    "0x.. master key ", //    _owner,
+    "0x.. server key ", //    _stable,
+    "0x.. server key", //    _beta,
+    "0x.. server key", //    _nightly,
+    "0x.. manual key", //    _stableConfirmer,
+    "0x.. manual key", //    _betaConfirmer,
+    null, //  _nightlyConfirmer,
+    "0x.. address of operations contract")
+    .then(() => console.log("OperationsProxy address: " + OperationsProxy.address))
 };
